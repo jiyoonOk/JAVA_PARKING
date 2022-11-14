49e702742894f09f865d456b6527e11042461c44
Codingjieun <95678789+Codingjieun@users.noreply.github.com>
2022-11-14 11:43:39 +0900

    hi i'm jieun

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("I LOVE TEAM!");
         System.out.println("skrrrrrrrr");
+        System.out.println("hi i am jieun");
     }
    
 }
